16d7f01a6aa35b0524d9f1341083374e0f0971c9
Augustine Ikenwa <ajohn9353@gmail.com>
2022-09-19 00:08:24 +0100

    Modified challenge-5.js

--- algorithms/challenge-5.js
+++ algorithms/challenge-5.js
@@ -1,5 +1,20 @@
 function accum(str) {
- 
- }
+    let result = [];
+    let lowStr = str.toLowerCase();
+    
+    let i = 0;
+    while (i < str.length){
+        let newStr = lowStr[i].toUpperCase();
+        let j = 0;
+        while (j < i) {
+            newStr += lowStr[i]
+            j++;
+        }
+        result.push(newStr);
+        i++;
+    }
+    return result.join("-");
+}
 
-  module.exports =accum
+console.log(accum("RqaEzty"));
+module.exports =accum
