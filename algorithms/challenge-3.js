0af28f31c2e7b49157db7e3ef9d446e0042bdfd4
Augustine Ikenwa <ajohn9353@gmail.com>
2022-09-19 00:31:54 +0100

    Modified challenge-3.js

--- algorithms/challenge-3.js
+++ algorithms/challenge-3.js
@@ -2,9 +2,10 @@ const letters = "abcdefghijklmnopqrstuvwxyz";
 function alphabetPosition(text) {
     let result = [];
     let lowText = text.toLowerCase();
+    let ans;
 
     for (let i = 0; i < text.length; i++){
-      let ans = letters.indexOf(lowText[i]) + 1;
+      ans = letters.indexOf(lowText[i]) + 1;
       result.push(ans);
     }
     return result.join(" ");
