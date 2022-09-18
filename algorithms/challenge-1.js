de0909e545478fe1681595916cf6b228a61b4444
Augustine Ikenwa <ajohn9353@gmail.com>
2022-09-18 11:57:29 +0100

    Edited challenge-1.js

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,4 +1,19 @@
 function sumOfMultiples(arr) {
-   
+   let total;
+
+   for (let i = 0; i < arr.length; i++){
+    if (i % 3 === 0 && i % 5 === 0) {
+        total += arr[i];
+    }
+   }
+   console.log(total);
 }
 module.exports = sumOfMultiples
+
+sumOfMultiples([6, 3, 0, 30, 7]);
+
+// Initialize empty array
+// Check if arr[i] is divisible by 3 and 5
+// Push into newArr[i]
+// Loop through newArr[i] 
+// Sum newArr[i]
