5129725652c73d5737a8ea79e55219878cd5c461
Augustine Ikenwa <ajohn9353@gmail.com>
2022-09-18 19:18:02 +0100

    Modified challenge-2.js

--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -1,6 +1,13 @@
 function sumOfTwoSmallestNums(arr){
- 
-}
+    let sum;
+    let newArr = [];
 
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push(arr[i]);
+    }
+    newArr.sort((a, b) => a - b);
+    sum = newArr[0] + newArr[1];
+    return sum;
+}
 
 module.exports = sumOfTwoSmallestNums
